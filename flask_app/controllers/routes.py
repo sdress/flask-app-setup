8f24ae30eb2d37be28c4eb345e42c7e82452b03a
sdress <sarah.dresser1@gmail.com>
2022-03-22 23:01:00 -0700

    adding imports

--- flask_app/controllers/routes.py
+++ flask_app/controllers/routes.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask import render_template, redirect, request, session
+from flask import render_template, redirect, request, session, flash
 from flask_bcrypt import Bcrypt        
 from flask_app.models.model_name import Class_Name
 
